4a84f6740d0fe788165aa1151dd94dc8e62af186
caishuxing <caishuxing@linggan.com>
2018-01-11 09:53:07 +0800

    支持本地插件

---
+++ buildsrc/src/main/groovy/com/cai/plugin2/SecondPlugin2.groovy
@@ -0,0 +1,12 @@
+package com.cai.plugin2
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class SecondPlugin2 implements Plugin<Project> {
+
+    void apply(Project project) {
+        System.out.println("========================");
+        System.out.println("这是第二个插件!");
+        System.out.println("========================");
+    }
+}
