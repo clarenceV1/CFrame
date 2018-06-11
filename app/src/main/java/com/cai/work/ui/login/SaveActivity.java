109f40809d80adb137992ed26783d1a8dc872895
caishuxing <caishuxing@linggan.com>
2018-06-11 21:08:57 +0800

    添加功能

--- app/src/main/java/com/cai/work/ui/login/SaveActivity.java
+++ app/src/main/java/com/cai/work/ui/login/SaveActivity.java
@@ -1,6 +1,18 @@
 package com.cai.work.ui.login;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
@@ -29,6 +41,7 @@ public class SaveActivity extends AppBaseActivity<SaveBinding> implements SaveVi
     SavePresenter presenter;
     @Inject
     ILoadImage imageLoader;
+    private static final int IMAGE = 1;
 
     @Override
     public void initDagger() {
@@ -42,6 +55,14 @@ public class SaveActivity extends AppBaseActivity<SaveBinding> implements SaveVi
 
     @Override
     public void initView() {
+        mViewBinding.commonHeadView.tvTitle.setText(getString(R.string.save_titile));
+        mViewBinding.commonHeadView.ivGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         User user = presenter.getUserInfo();
         mViewBinding.tvMobile.setText(user.getMobile());
 
@@ -68,6 +89,46 @@ public class SaveActivity extends AppBaseActivity<SaveBinding> implements SaveVi
                 presenter.quit();
             }
         });
+        mViewBinding.imgIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                if (ContextCompat.checkSelfPermission(SaveActivity.this,
+//                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+//                    ActivityCompat.requestPermissions(this,
+//                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_CALL_PHONE2);
+//
+//                } else {
+//
+//                }
+                takePhoto();
+            }
+        });
+    }
+
+    private void takePhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //获取图片路径
+        if (requestCode == IMAGE && resultCode == Activity.RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumns = {MediaStore.Images.Media.DATA};
+            Cursor c = getContentResolver().query(selectedImage, filePathColumns, null, null, null);
+            c.moveToFirst();
+            int columnIndex = c.getColumnIndex(filePathColumns[0]);
+            String imagePath = c.getString(columnIndex);
+            showImage(imagePath);
+            c.close();
+        }
+    }
+
+    private void showImage(String imaePath) {
+        Bitmap bm = BitmapFactory.decodeFile(imaePath);
+        mViewBinding.imgIcon.setImageBitmap(bm);
     }
 
     @Override
