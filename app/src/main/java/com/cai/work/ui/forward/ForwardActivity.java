d3bac775196a88375a7d19fc96077e5b54de0df1
caishuxing <caishuxing@linggan.com>
2018-06-24 17:09:03 +0800

    添加刷新

--- app/src/main/java/com/cai/work/ui/forward/ForwardActivity.java
+++ app/src/main/java/com/cai/work/ui/forward/ForwardActivity.java
@@ -15,6 +15,7 @@ import com.cai.work.R;
 import com.cai.work.base.AppBaseActivity;
 import com.cai.work.bean.Forward;
 import com.cai.work.bean.ForwardRecord;
+import com.cai.work.bean.Record;
 import com.cai.work.dagger.component.DaggerAppComponent;
 import com.cai.work.databinding.ForwardBinding;
 import com.example.clarence.utillibrary.DimensUtils;
@@ -34,11 +35,13 @@ public class ForwardActivity extends AppBaseActivity<ForwardBinding> implements 
     ForwardAdapter adapter;
     SpinerPopWindow spinerPopWindow;
     int spinerPopwindowHeight;
+    Forward forward;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         ARouter.getInstance().inject(this);
         super.onCreate(savedInstanceState);
+        spinerPopwindowHeight = DimensUtils.dip2px(this, 350);
     }
 
     @Override
@@ -58,19 +61,28 @@ public class ForwardActivity extends AppBaseActivity<ForwardBinding> implements 
 
     @Override
     public void initView() {
-        spinerPopwindowHeight = DimensUtils.dip2px(this, 300);
-        mViewBinding.ivGoBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-        mViewBinding.tvRule.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ARouter.getInstance().build("/AppModule/WebActivity").withCharSequence("url", "http://m.hellceshi.com/tpl/app/futures_rule.html").withCharSequence("title", "期货规则").navigation();
-            }
-        });
+        initData();
+        initHead();
+        initSpinner();
+        refreshView();
+    }
+
+    private void refreshView() {
+        if (forward != null) {
+            mViewBinding.tvTitle.setText(forward.getName());
+            mViewBinding.tvName.setText(forward.getName());
+            mViewBinding.tvCode.setText("(" + forward.getCode() + ")");
+        }
+        requestData();
+    }
+
+    private void initData() {
+        if (!TextUtils.isEmpty(forwardJson)) {
+            forward = JSON.parseObject(forwardJson, Forward.class);
+        }
+    }
+
+    private void initSpinner() {
         mViewBinding.spinner.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -83,18 +95,33 @@ public class ForwardActivity extends AppBaseActivity<ForwardBinding> implements 
         spinerPopWindow.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Forward forward = (Forward) adapter.getItem(position);
-                mViewBinding.tvTitle.setText(forward.getName());
+                forward = (Forward) adapter.getItem(position);
+                refreshView();
                 spinerPopWindow.dismiss();
             }
         });
-        if (!TextUtils.isEmpty(forwardJson)) {
-            Forward forward = JSON.parseObject(forwardJson, Forward.class);
-            mViewBinding.tvTitle.setText(forward.getName());
+    }
+
+    private void requestData() {
+        if (forward != null) {
             presenter.requestRecord(forward.getCode());
         }
-
         presenter.requestContracts();
+    }
+
+    private void initHead() {
+        mViewBinding.ivGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        mViewBinding.tvRule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ARouter.getInstance().build("/AppModule/WebActivity").withCharSequence("url", "http://m.hellceshi.com/tpl/app/futures_rule.html").withCharSequence("title", "期货规则").navigation();
+            }
+        });
     }
 
     //设置PopWindow
@@ -115,7 +142,10 @@ public class ForwardActivity extends AppBaseActivity<ForwardBinding> implements 
 
     @Override
     public void callBack(ForwardRecord forwardRecord) {
-
+        Record record = forwardRecord.getRecords();
+        if (record != null) {
+            mViewBinding.tvTradeTime.setText(record.getBuyDate());
+        }
     }
 
     @Override
